ce92c03cf2560bc78354afc93cab2b02eabccd07
Irene Ku Iveling <82irekui@skola.boras.se>
2020-09-29 08:46:52 +0200

    Start på H5Chocolat

---
+++ Chocolate.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+class Chocolate
+{
+    string name;
+    int cacaoAmount;
+    int milkAmount;
+    List<string> filling;
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H5Chocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to H5Chocolate!");
+        }
+    }
+}
